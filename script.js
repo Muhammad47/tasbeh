6f8ea2b0bcf7cc50c319634a0815d01040e81e8d
Muhammad47 <muhammadge4@gmail.com>
2022-09-15 15:42:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -21,23 +21,34 @@ function clickNo() {
 }
 // night or sun
 function Night() {
+  //background
   document.getElementById("mrmge").style.backgroundColor = "black";
+  //select element
   document.getElementById("selecto").style.backgroundColor = "black";
   document.getElementById("selecto").style.color = "white";
+  //button 
   document.getElementById("butt").style.backgroundColor = "black";
   document.getElementById("butt").style.color = "white";
   document.getElementById("butt").style.border = "1px white solid";
+  //number color
+   document.getElementById("clicks").style.color = "White";
+  //icons
   document.getElementById("night").style.display = "none";
   document.getElementById("sun").style.display = "inline";
 }
 function Sun() {
+    //background
   document.getElementById("mrmge").style.backgroundColor = "white";
+  //select element
   document.getElementById("selecto").style.backgroundColor = "white";
   document.getElementById("selecto").style.color = "black";
-  document.getElementById("clicks").style.color = "black";
+  //button 
   document.getElementById("butt").style.backgroundColor = "#E5E5E5";
   document.getElementById("butt").style.color = "black";
   document.getElementById("butt").style.border = "1px black solid";
+  //number color
+   document.getElementById("clicks").style.color = "black";
+  //icons
   document.getElementById("night").style.display = "inline";
   document.getElementById("sun").style.display = "none";
 }
